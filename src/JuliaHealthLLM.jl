0537904d0ce96385830307864d4d7129ea0f7bea
paramthakkar123 <paramthakkar864@gmail.com>
2025-04-09 22:40:52 +0530

    Added code for chunking the knowledge corpus

--- src/JuliaHealthLLM.jl
+++ src/JuliaHealthLLM.jl
@@ -4,9 +4,10 @@ using LibGit2
 using Downloads
 using Dates
 using TextAnalysis
-using Serialization
+using Serialization, Base.Threads
 using Transformers, CUDA
 using LlamaCpp, JSON3, HuggingFaceTokenizers, Jjama3
+using JSON, Markdown
 
 # TODO: Add functions from scripts
 include("../scripts/clone.jl")
@@ -15,6 +16,7 @@ include("../scripts/restore_submodules.jl")
 include("../scripts/update_julia_package_repos.jl")
 include("../scripts/update_and_record.jl")
 include("../scripts/load_model.jl")
+include("chunk_docs.jl")
 
 export clone_repositories, normalize_repo_url, clone_repo, is_textual_file,
        build_corpus_from_repo, save_corpus, process_directory, update_and_record_submodules,
